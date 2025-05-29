903dd8347908941bbb7fc30b6c88e1a37cc8e741
LeonardoAVONI <avonileonardo@gmail.com>
2025-05-29 15:42:27 +0200

    Added comments to WingLoop

--- WingLoop_Library/WingLoop_Library.py
+++ WingLoop_Library/WingLoop_Library.py
@@ -1,46 +1,48 @@
 """
 ====================================================================================
-Controlled Flight Text, Version 2
+WingLoop, Package Version
 
 Author: Leonardo AVONI
 Date: 25/10/2024
 Email: avonileonardo@gmail.com
 
-Last modified: 17/12/2024
-quick test
+Last modified: 29/05/2025
 ====================================================================================
 
 Description:
-This Python code is made to be used with Aswing_Director library. 
-Its goal is to perform a time-transient simulation on a flexible hawk aircraft
-The code runs as follows
-    First the script settings and simulation parameters are specified (Ts, N, K, L, Dt...)
-    The PID parameters are specified
-    Then graphics are deactivated
-    Then the initial state is imported
-    THe simulation settings are imported
-    A 1-cosine gust profile is imported
-    Ground effect is disabled
-    Altitude units are set to km
-    Also, the output text file is made lighter by not-showing beam-wise variables
-    Then a first L iterations are computed
-    Then the other iterations are performed by groups of K iterations at a time, with control (either via matlab or via python)
-    Then the results can be plotted and/or the score can be displayed
-    The time results are also written to a csv file for easy access
+    This code is made for aeroservoelastic control of aircraft modeled using ASWING.
+    It is meant as a way to extend ASWING's control capabilities using the toolboxes
+    available in Python. It is also possible, although not implemented, to link Python
+    with Matlab, thus implementing the methods available in Matlab.
+    
+    This Python code is made to be used with Aswing_Director library, used to send commands 
+    to ASWING from Python. This code also uses functions from the following classes:
+        Control_Library.PyControl: where all the timeseries and control laws to be used are stored
+        Text2Python_Library: used to convert the python data to text data accessible by ASWING, 
+            and vice-versa
+    
+    The WingLoop class comes with several methods:
+        -Launch_ASWING: used to start ASWING with the appropriate parameters, like the file used and 
+            the ASWING version used, and other parameters
+        -Load_Files: used to load the various files needed by ASWING. The files are loaded by type according
+            to their extension (.asw will be geometry, .state will be a state...)
+        -Deactivate_Graphics: specific list of instructions for ASWING, to stop if from showing graphic 
+            output. ASWING can still save images if needed, but those are not showed to the user
+        -Time_Transient_Simulation: used to launch a time transient simulation. This function 
+            calls Perform_K_iterations
+        -Outputting_The_Results: plots and/or writes timeseries
+        -Closing_WingLoop: does what it says
 
+    More details are found in each function's description
 
-
-Features:
--control in matlab and python
--Aswing_V2 use (modified to go beyond 201 op.points limit)
--time measurements (to check which part of the code take how much time)
--added the possibility to use angular velocity from ASWING, and increased read/write precision using ASWING V3
--added ASWING output file checks, avoiding crashes
-
-Next improvements:
--Simulink Bridge
--independence on the aircraft used
--better organization of the control part, making a better class
+How To Install
+    Position yourself in the folder upstream of WingLoop_Library, i.e. the 
+    folder containing this WingLoop_Library.py code, and the 
+    Text2Python_Library.py, Control_Library.py codes.
+    Alongside that folder there should be the pyproject.toml
+    Open a terminal, and write "/usr/bin/python3 -m pip install --user -e ."
+    This will install it in Python, in editable mode (if you modify the code it 
+    will impact the package behavior)
 
 ====================================================================================
 """
@@ -62,20 +64,22 @@ import sys
 
 class WingLoop:
     def __init__(self):
-        """_summary_
-        
-        Initializes a class responsibe of handling an ASWING instance
+        """
+        Declares the WingLoop class
         """
         pass
     
-    def Launch_ASWING(self,aswing_path,sim_directory,asw_filename,print_output,timer_text=0.000001,finished_writing_check_timestep=0.001):
-        """_summary_
-        aswing_path: path at the end of which there's the used ASWING executable to use
-        sim_directory: directory where the simulation files are, with-respect-to current working directory
-        asw_filename: .asw filename to use
-        print_output: True or False: if True, WingLoop data will be printed to the console
-        check_timestep: timer used to check for the last update on the size of the state file written by ASWING
-        timer_text: timer used by default for the Aswing_Director functions
+    def Launch_ASWING(self,aswing_path,sim_directory,asw_filename,print_output,
+                      timer_text=0.000001,
+                      finished_writing_check_timestep=0.001):
+        """
+        
+        aswing_path (str): path at the end of which there's the ASWING executable to use
+        sim_directory (str): local directory where the simulation files are
+        asw_filename (str): .asw filename to use
+        print_output (bool): if True, WingLoop data will be printed to the terminal
+        timer_text (float): timer used by default for the Aswing_Director functions
+        finished_writing_check_timestep (float): timer used to check for the last update on the size of the state file written by ASWING
         """
         # storing the variables to self.
         self.sim_directory = sim_directory
@@ -98,9 +102,10 @@ class WingLoop:
         
                 
     def Load_Files(self,filename):
-        """_summary_
-        From the main ASWING menu, this function loads the file called "filename", according to its extension
-        When using ASWING from the terminal, any file extension can be loaded. Here the extension will make the difference
+        """
+        From the main ASWING menu, this function loads the file called "filename", 
+        according to its extension. When using ASWING from the terminal, any file 
+        extension can be loaded. Here the extension will make the difference
 
         Args:
             filename (string): filename to enter in ASWING from the main menu, including the extension
@@ -121,7 +126,9 @@ class WingLoop:
             sys.exit()
             
     def Deactivate_Graphics(self):
-# Deactivate Graphics
+        """
+        Defines the list of ASWING instructions needed to deactivate the graphical output of ASWING
+        """
         stdout, stderr = self.ASW_handler.send_command_and_receive("PLPA")
         stdout, stderr = self.ASW_handler.send_command_and_receive("G")
         stdout, stderr = self.ASW_handler.send_command_and_receive("\n")
@@ -131,13 +138,13 @@ class WingLoop:
         """
         Works from the Oper menu, made to append K_aswing iterations with control to current simulation
         The function does the following:
-            writes current state from ASWING to a .txt file
-            obtains from such state the (instantaneous) variables used for control
-            appends the instantaneous variables to the previous time series of previously appended values
-                this operation can be done in Matlab or Python, but here it's done in Python
-            asking the correspondent control function for the controls needed, according to a control scheme defined in Control_Library
-            writing a text file to be used by ASWING for control (defining elevator inputs, engine input...)
-            send K_aswing iterations in ASWING with the generated control text file
+            -writes current "output" state from ASWING to a text file
+            -obtains from such state the (instantaneous) variables used for control
+            -appends the instantaneous variables to the previous time series of previously appended values. 
+                This operation can be done Python, but here it's done in Python
+            -asking the PyControl.UAV_control_Strategy function for the controls needed at the next time step
+            -writing the "input" text file to be used by ASWING for control (defining elevator inputs, engine input...)
+            -send K_aswing iterations in ASWING, with Dt intervals with the generated "input" control text file
 
         """
         # we check if we are doing the very first time-transient simulation
@@ -204,7 +211,6 @@ class WingLoop:
         
         #instantaneous_flight_data = text2python_main("output")
         instantaneous_flight_data = text2python_withderivative("output")    
-        print(instantaneous_flight_data)
         
         # Appending the data in the python script
         self.PyControl.append_flight_data(instantaneous_flight_data)
@@ -231,30 +237,42 @@ class WingLoop:
         pass
 
     def Setting_Trimming_Point_Constraints():
-        # Would be nice to have it for automation
+        # Would be nice to have it for automation, but not defined at the moment
+        # it would work using OPER>% not OPER>T
         pass
 
     def Time_Transient_Simulation(self,Dt,N,K):
-        """
-        After having loaded the needed settings, and having imported the .state file 
-        for the trimming point infos,
-        the program moves in OPER
-        changes the simulation to time-transient
-        and solves it by looping it with Python
-        """
         
-        # Perform Simulations in Oper
-        """
-        load the set of constraints needed for trimmed flight computation
-        instead of building them one by one in the OPER>T menu
-
-        We will here load a .state file including
-            -the free flight constraints
-            -the fixed-stick trimmed aircraft constrol surface deflections and engine power
-        The .state file does not contain the simulation specifics i.e. steady or unsteady
-
-        The trimmed .state file, for the moment, must be created manually
-        """
+        """ 
+        Simulates the time-transient behavior of the aircraft starting from a pre-computed trimming point.
+        This method performs a time-transient simulation in ASWING by iteratively solving the system dynamics.
+        It begins by loading the necessary settings and importing a `.state` file containing the trimming point
+        information. The `.state` file includes generally: constraints for free flight, fixed-stick trimmed control surface
+        deflections, and engine power but is defaulted to "steady" simulation
+        The simulation is divided into three phases:
+        1. **Initial Iteration**: Performs the first set of L iterations starting from the trimming point. In general, L=K
+        2. **Intermediate Iterations**: Loops through the simulation in chunks of `K` iterations until reaching just before
+            the total number of iterations `N`.
+        3. **Final Iteration**: Completes the remaining iterations to reach the total count `N`.
+        Parameters:
+            Dt (float): The time step for the simulation, in seconds
+            N (int): The total number of iterations to perform.
+            K (int): The number of iterations to perform in each chunk
+        Notes:
+            - The `.state` file must be created manually and loaded before running this simulation.
+            - The method assumes that the ASWING handler (`self.ASW_handler`) is properly initialized and
+                capable of sending commands to ASWING.
+            - The `self.count` variable tracks the number of iterations already performed.
+        Attributes:
+            self.count (int): Tracks the number of iterations performed so far.
+            self.print_output (bool): If True, prints progress information during the simulation.
+        Workflow:
+            - Switches ASWING to the `OPER` menu and sets the simulation mode to time-transient.
+            - Performs the initial iteration using the trimming point.
+            - Executes intermediate iterations in chunks of `K`.
+            - Completes the final round of iterations to reach the total count `N`.
+            - Optionally prints progress and outputs simulation data.
+        """        
 
         # Go to OPER, time-transient simulation menu
         stdout, stderr = self.ASW_handler.send_command_and_receive("oper") #go to oper menu
